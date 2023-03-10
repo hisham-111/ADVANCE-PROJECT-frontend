632ecf56aa058da932c8da02dba695548b23fae3
fatima-katrib <fatimakatrib19@gmail.com>
2023-03-10 09:34:17 +0200

    add charts

---
+++ financial-app/src/components/charts/balanceBarChart/balanceBarChart.js
@@ -0,0 +1,49 @@
+// import React from 'react';
+// import { Bar } from 'react-chartjs-2';
+// import { makeStyles } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   },
+// }));
+
+// const data = {
+//   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//   datasets: [
+//     {
+//       label: 'Balance',
+//       data: [500, 600, 700, 800, 900, 1000, 1100],
+//       backgroundColor: 'rgba(54, 162, 235, 0.5)',
+//       borderColor: 'rgba(54, 162, 235, 1)',
+//       borderWidth: 1,
+//     },
+//   ],
+// };
+
+// const options = {
+//   scales: {
+//     yAxes: [
+//       {
+//         ticks: {
+//           beginAtZero: true,
+//         },
+//       },
+//     ],
+//   },
+// };
+
+// const BalanceChart = () => {
+//   const classes = useStyles();
+
+//   return (
+//     <Paper className={classes.paper}>
+//       <Bar data={data} options={options} />
+//     </Paper>
+//   );
+// };
+
+// export default BalanceChart;
