a08fba31da71ff8fb63243901e77cb74bd740f3b
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-03-11 10:50:35 +0200

    edit on transaction appJS

--- financial-app/src/App.js
+++ financial-app/src/App.js
@@ -1,35 +1,16 @@
-// import logo from './logo.svg';
 import './App.css';
 
-import AllRoutes from './components/routes';
-// import { ThemeProvider } from '@mui/material/styles';
-// import { ColorModeContext, useMode } from "./theme";
-// import CssBaseline from '@mui/material/CssBaseline';
-// import { MuiTable } from './components/transaction/transaction';
 
 
 function App() {
-  // const [theme, colorMode] = useMode();
 
-  // useEffect(() => {
-  //   const mode = localStorage.getItem("colorMode");
-  //   if (mode) {
-  //     colorMode.toggle();
-  //   }
-  // }, []);
   
   return (
     <div className="App">
-          {/* <ColorModeContext.Provider value={colorMode}> */}
-          {/* <ThemeProvider theme={theme}> */}
-          {/* <CssBaseline /> */}
-      <AllRoutes />
-      
-      {/* </ThemeProvider> */}
-      {/* </ColorModeContext.Provider> */}
+        
 
 
-      
+    
 
     </div>
   );
