5e943979e3be8ca7194291887bd4c1167bcbee80
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2023-03-12 10:51:20 +0200

    added the CategroriesBox

--- financial-app/src/App.js
+++ financial-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
-
+// import Categories from './components/Categories/Categories_History';
+import Categories_History from './components/Categories/Categories_History'
 function App() {
 
   
   return (
     <div className="App">
+
+     <Categories_History />
 
     </div>
   );
