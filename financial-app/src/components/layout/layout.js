3b7bcf37c7f63a20fd36859fe1264035794633e9
fatima-katrib <fatimakatrib19@gmail.com>
2023-03-13 10:41:30 +0200

    change Layout component

--- financial-app/src/components/layout/layout.js
+++ financial-app/src/components/layout/layout.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import NavBar from '../sideBar/sideBar.js'
+import SideBar from '../sideBar/sideBar.js';
+import Header from '../Header/Header'
 import './layout.css';
+import { Container } from '@mui/material';
+import { Stack } from '@mui/system';
 
-export default function Layout() {
+export default function Layout({children}) {
   return (
-    <div className="layout">
-    <NavBar />
+    <div className="layout" style={{display:"flex", justifyContent:"space-between"}}>
+      
+    <SideBar />
+    <Container>
+    <Header/>
     {children}
+    </Container>
   </div>
   )
 }
