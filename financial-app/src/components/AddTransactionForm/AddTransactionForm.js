bdc2d737cc7c4a612c3a6492a92f33c189f18338
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-03-14 14:45:00 +0200

    without submit function

--- financial-app/src/components/AddTransactionForm/AddTransactionForm.js
+++ financial-app/src/components/AddTransactionForm/AddTransactionForm.js
@@ -4,7 +4,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { FormControl, InputLabel, Select, MenuItem,TextField } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem,TextField, Button } from '@mui/material';
 import { useState } from 'react';
 import { Stack } from '@mui/system';
 
@@ -43,18 +43,25 @@ function a11yProps(index) {
 
 export default function AddTransactionForm() {
   const [value, setValue] = useState(0);
-  const [TypeCode, setTypeCode] = useState('');
+  // const [TypeCode, setTypeCode] = useState('');
   const [currency,SetCurrency] = useState('');
   const [amount, setAmount] = useState(0);
   const [schedule, setSchedule] = useState('');
   const [description, setDescription] = useState('');
+  // const [category, setCategory] = useState('');
+
+  const categoriesByType = {
+    Incomes: ['Bills'],
+    Expenses: ['Electricity', 'Building'],
+  };
+  const [typeCode, setTypeCode] = useState('Incomes');
+  const [category, setCategory] = useState('');
+
+  const availableCategories = categoriesByType[typeCode];
   const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleTypeCodeChange = (event) => {
-    setTypeCode(event.target.value);
-  };
   const handleCurrencyChange = (event) =>{
     SetCurrency(event.target.value);
   }
@@ -70,10 +77,23 @@ export default function AddTransactionForm() {
   const handleDescriptionChange = (event) =>{
     setDescription(event.target.value);
   }
+ 
+  
+  const handleTypeCodeChange = (event) => {
+    setTypeCode(event.target.value);
+    setCategory('');
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  
+
   return (
     <Box sx={{ width: '100%' }}>
   <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-    <Typography style={{ fontWeight: '600', color: '#394452', fontSize: '27px' }}>
+    <Typography style={{ fontWeight: '600', color: '#394452', fontSize: '27px',paddingTop:"6px",paddingLeft:"8px" }}>
       Add New <Box display="inline" style={{ color: '#026FC2' }}>Transaction</Box>
     </Typography>
     <Tabs value={value} onChange={handleTabChange} aria-label="basic tabs example">
@@ -84,74 +104,117 @@ export default function AddTransactionForm() {
 
 
   <TabPanel value={value} index={0} >
-    <Stack spacing={2} style={{width:"calc(100% - 20%)"}}>
-        
-        <TextField variant='outlined' label="Title" type="text" size="10" required/>
-          <TextField type="date" label="Start-Date" autoFocus focused />
-        
+    <Stack spacing={4} >
+        <Stack  display="flex" flexDirection="row" justifyContent="space-between">
+        <TextField variant='outlined' label="Title" type="text" size="10" required style={{width:"70%"}}/>
 
-        <FormControl sx={{ minWidth: 120,width:"30px" }}> 
+        <FormControl  style={{width:"25%" ,margin:"0"}}>
+          <InputLabel id="schedule-select-label">Schedule</InputLabel>
+            <Select style={{margin:"0"}}
+              labelId="schedule-select-label"
+              id="schedule-select"
+              value={schedule}
+              label="Schedule"
+              onChange={handleScheduleChange} >
+                  
+              <MenuItem value="Weekly">Weekly</MenuItem>
+              <MenuItem value="Monthly">Monthly</MenuItem>
+              <MenuItem value="Yearly">Yearly</MenuItem>
+            </Select>
+       </FormControl>
+       </Stack>
+
+       <Stack  display="flex" flexDirection="row" justifyContent="space-between">
+          <TextField type="date" label="Start-Date" autoFocus focused style={{width:"70%"}}/>
+        <FormControl style={{width:"25%"}}> 
           <InputLabel>TypeCode</InputLabel>
           <Select
             labelId="TypeCode-select-label"
             id="TypeCode-select"
-            value={TypeCode}
-            label="TypeCode"
-            onChange={handleTypeCodeChange}>
+            value={typeCode}
+          label="TypeCode"
+          onChange={handleTypeCodeChange}>
             <MenuItem value="Incomes">Income</MenuItem>
             <MenuItem value="Expenses">Expenses</MenuItem>
             </Select>
         </FormControl>
+        </Stack>
 
-        <FormControl sx={{ minWidth: 120,width:"30px" }}> 
-          <InputLabel>Currency</InputLabel>
-          <Select
-            labelId="Currency-select-label"
-            id="Currency-select"
-            value={currency}
-            label="Currency"
-            onChange={handleCurrencyChange}>
-            <MenuItem value="L.L">L.L.</MenuItem>
-            <MenuItem value="$">$</MenuItem>
+        <Stack  display="flex" flexDirection="row" justifyContent="space-between">
+          <TextField type="number" variant='outlined' value={amount} label='Amount'  onChange={handleAmountChange} inputProps={{steps:10 }} style={{width:"45%"}}/>  { /*set the increment/decrement step to 100*/}
+          <FormControl style={{width:"25%"}}> 
+            <InputLabel>Currency</InputLabel>
+            <Select
+              labelId="Currency-select-label"
+              id="Currency-select"
+              value={currency}
+              label="Currency"
+              onChange={handleCurrencyChange}>
+              <MenuItem value="L.L">L.L.</MenuItem>
+              <MenuItem value="$">$</MenuItem>
 
-            </Select>
-        </FormControl>
+              </Select>
+          </FormControl>
+          <FormControl style={{width:"25%"}}>
+          <InputLabel id="category-select-label">Category</InputLabel>
+            <Select
+              labelId="category-select-label"
+              id="category-select"
+              value={category}
+              label="Category"
+              onChange={handleCategoryChange}
+            >
+              {availableCategories.map((category) => (
+                <MenuItem key={category} value={category}>
+                  {category}
+                </MenuItem>
+              ))}
+              </Select>
+          </FormControl>
+        </Stack>
 
-        <TextField type="number" variant='outlined' value={amount} label='Amount'  onChange={handleAmountChange} inputProps={{steps:10 }}/>  { /*set the increment/decrement step to 100*/}
       
-      <FormControl sx={{ m: 1, minWidth: 10 }}>
-      <InputLabel id="schedule-select-label">Schedule</InputLabel>
-        <Select
-          labelId="schedule-select-label"
-          id="schedule-select"
-          value={schedule}
-          label="Schedule"
-          onChange={handleScheduleChange} >
-              
-          <MenuItem value="Weekly">Weekly</MenuItem>
-          <MenuItem value="Monthly">Monthly</MenuItem>
-          <MenuItem value="Yearly">Yearly</MenuItem>
-        </Select>
-    </FormControl>
     <TextField value={description} placeholder="Description" label="Description" onChange={handleDescriptionChange}  variant='outlined' multiline rows={4}  rowsMax={8}/>
+    
+                <Stack display="flex" flexDirection="row" justifyContent="center">
+                  <Button 
+                  style={{ backgroundColor: "#026FC2",
+                          width: "200px",
+                          borderRadius: '10px',
+                          color:"#FFF",
+                          fontWeight:"600",
+                          padding:"10px" }}
+                          variant="outlined"
+                      >Save</Button>
+                    </Stack>
+        
 
+        
     </Stack>
+    
   </TabPanel>
 
 
+
+
+
+
   <TabPanel value={value} index={1}>
-  <Stack spacing={2} style={{width:"calc(100% - 20%)"}}>
+  <Stack spacing={3} >
         
         <TextField variant='outlined' label="Title" type="text" size="10" required/>
-          <TextField type="date" label="Start-Date" autoFocus  focused/>
-          <TextField type="date" label="End-Date" autoFocus  focused/>
+          <Stack display="flex" flexDirection="row" justifyContent="space-between">
+            <TextField type="date" label="Start-Date" autoFocus  focused style={{width:"48%"}}/>
+            <TextField type="date" label="End-Date" autoFocus  focused   style={{width:"48%"}}/>
+          </Stack>
 
-        <FormControl sx={{ minWidth: 120,width:"30px" }}> 
+          <Stack display="flex" flexDirection="row" justifyContent="space-between"> 
+        <FormControl sx={{ width:"48%" }}> 
           <InputLabel>TypeCode</InputLabel>
           <Select
             labelId="TypeCode-select-label"
             id="TypeCode-select"
-            value={TypeCode}
+            value={typeCode}
             label="TypeCode"
             onChange={handleTypeCodeChange}>
             <MenuItem value="Incomes">Income</MenuItem>
@@ -159,24 +222,53 @@ export default function AddTransactionForm() {
             </Select>
         </FormControl>
 
-        <FormControl sx={{ minWidth: 120,width:"30px" }}> 
-          <InputLabel>Currency</InputLabel>
-          <Select
-            labelId="Currency-select-label"
-            id="Currency-select"
-            value={currency}
-            label="Currency"
-            onChange={handleCurrencyChange}>
-            <MenuItem value="L.L">L.L.</MenuItem>
-            <MenuItem value="$">$</MenuItem>
+          <FormControl style={{width:"48%"}}>
+            <InputLabel id="category-select-label">Category</InputLabel>
+              <Select
+                labelId="category-select-label"
+                id="category-select"
+                value={category}
+                label="Category"
+                onChange={handleCategoryChange}
+              >
+                {availableCategories.map((category) => (
+                  <MenuItem key={category} value={category}>
+                    {category}
+                  </MenuItem>
+                ))}
+                </Select>
+            </FormControl>
+                </Stack>
 
-            </Select>
-        </FormControl>
-
-        <TextField type="number" variant='outlined' value={amount} label='Amount'  onChange={handleAmountChange} inputProps={{steps:10 }}/>  { /*set the increment/decrement step to 100*/}
+          <Stack display="flex" flexDirection="row" justifyContent="space-between">  
+              <TextField type="number"  variant='outlined' value={amount} label='Amount'  onChange={handleAmountChange} inputProps={{steps:10 }} style={{width:"65%"}}/>  { /*set the increment/decrement step to 100*/}
+              <FormControl style={{width:"30%"}}> 
+                <InputLabel>Currency</InputLabel>
+                <Select
+                  labelId="Currency-select-label"
+                  id="Currency-select"
+                  value={currency}
+                  label="Currency"
+                  onChange={handleCurrencyChange}>
+                  <MenuItem value="L.L">L.L.</MenuItem>
+                  <MenuItem value="$">$</MenuItem>
+                  </Select>
+              </FormControl>
+                  </Stack>  
       
       
     <TextField value={description} placeholder="Description" label="Description" onChange={handleDescriptionChange}  variant='outlined' multiline rows={4}  rowsMax={8}/>
+    <Stack display="flex" flexDirection="row" justifyContent="center">
+                  <Button 
+                  style={{ backgroundColor: "#026FC2",
+                          width: "200px",
+                          borderRadius: '10px',
+                          color:"#FFF",
+                          fontWeight:"600",
+                          padding:"10px" }}
+                          variant="outlined"
+                      >Save</Button>
+                    </Stack>
     </Stack>
   </TabPanel>
 </Box>
