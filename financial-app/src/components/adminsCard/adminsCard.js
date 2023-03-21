93cb8b73960760f3217ea8dfbe5957228708b519
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-03-21 18:54:49 +0200

    finishing edit admin

--- financial-app/src/components/adminsCard/adminsCard.js
+++ financial-app/src/components/adminsCard/adminsCard.js
@@ -1,74 +1,92 @@
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  Stack,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
-import { Box, Button, Divider, Typography,DialogActions,DialogTitle,Dialog,TextField,DialogContent } from "@mui/material";
-import { Stack } from "@mui/system";
-import { React, useState,useEffect } from "react";
-import axios from 'axios';
-export default function AdminsCard() {
+import axios from "axios";
+
+const AdminsList = () => {
+  const [admins, setAdmins] = useState([]);
   const [open, setOpen] = useState(false);
   const [formValues, setFormValues] = useState({
-    name: '',
-    email: '', 
+    id: "",
+    name: "",
+    email: "",
   });
-  
-
-    const [admins, setAdmins]=useState([
-        {
-            id: "1",
-            fullName: "Admin Name-1",
-            userName: "Admin userName-1",
-            created_at: "yy-mm-dd",
-        },
-        {
-            id: "2",
-            fullName: "Admin Name-2",
-            userName: "Admin userName-2",
-            created_at: "yy-mm-dd",
-        }
-    ]);
-
-    
-
-
-    const handleSubmit = (event,id) => {
-      event.preventDefault();
-      const data = {
-        name: formValues.name,
-        email: formValues.email,
-        
-      };
-      axios
-      .patch(`http://localhost:8000/api/auth/admin/${id}`,data, {
+ 
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/auth/admins", {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("access_token"),
         },
       })
       .then((response) => {
-        // If the deletion was successful, update the list of admins
-        setAdmins(admins.filter((item) => item.id !== id));
-        console.log(response.data);
-        setFormValues({
-          name: '',
-          email: '',
-          password: '',
-        })
-        handleClose();
+        setAdmins(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-    };
+  }, []);
+ 
+  const handleClickOpen = (id) => {
+    // Open the edit dialog and populate the form with the data of the selected admin
+    const selectedAdmin = admins.find((admin) => admin.id === id);
+    setFormValues({
+      id: selectedAdmin.id,
+      name: selectedAdmin.name,
+      email: selectedAdmin.email,
+    });
+    setOpen(true);
+    
+  };
 
+  const handleClose = () => {
+    // Close the edit dialog and reset the form values
+    setFormValues({
+      id: "",
+      name: "",
+      email: "",
+    });
+    setOpen(false);
 
+  };
 
+  const handleEdit = (event) => {
+  event.preventDefault();
+  const id = formValues.id;
+  const data = {
+    name: formValues.name,
+    email: formValues.email,
+  };
+  axios
+    .patch(`http://localhost:8000/api/auth/admin/${id}`, data, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("access_token"),
+      },
+    })
+    .then((response) => {
+      // If the update was successful, update the list of admins and close the dialog
+      const updatedAdmins = updatedAdmins.map((admin) =>
+        admin.id === id ? response.data : admin
+      );
+      setAdmins(updatedAdmins);
+      handleClose();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-  function handleDelete(id) {
+  const handleDelete = (id) => {
     axios
       .delete(`http://localhost:8000/api/auth/admin/${id}`, {
         headers: {
@@ -77,31 +95,14 @@ export default function AdminsCard() {
       })
       .then((response) => {
         // If the deletion was successful, update the list of admins
-        setAdmins(admins.filter((item) => item.id !== id));
-        console.log(response.data);
+        setAdmins(admins.filter((admin) => admin.id !== id));
       })
       .catch((error) => {
         console.log(error);
       });
-  }
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/auth/admins',{
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-      }
-    })
-      .then(response => {
-        setAdmins(response.data);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },[]);
+  };
 
   return (
-    
     <Box
       sx={{
         padding: "50px",
@@ -109,132 +110,129 @@ export default function AdminsCard() {
         border: "1px solid rgba(109, 125, 147, 0.15)",
         boxShadow: "4px 4px 20px -10px rgba(0, 0, 0, 0.1)",
         borderRadius: "12px",
-        bgcolor:"white",
+        bgcolor: "white",
       }}
     >
-      <Typography variant="h6" sx={{ fontWeight: "bold" }} marginBottom={1}>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }}>
         Admins
       </Typography>
-      <Stack spacing="20px">
-        {admins
-          .map((item) => {
-            return (
-              <>
-                <Stack
-                  direction="row"
-                  justifyContent="space-between"
-                  alignItems="center"
-                  key={item.id}
-                >
-                  <Box
-                    sx={{
-                      padding: "16px",
-                      borderRadius: "12px",
-                      maxWidth: "56px",
-                      maxHeight: "56px",
-                      bgcolor: "#E6F1F9",
-                    }}
-                  >
-                    <PersonRoundedIcon color="primary" />
-                  </Box>
-                  <Stack>
+      <Stack spacing="10px">
+      {admins.map((admin) => (
+      <Box
+        key={admin.id}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          py: 2,
+          borderBottom: "1px solid rgba(109, 125, 147, 0.15)",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box
+            sx={{
+              padding: "16px",
+              borderRadius: "12px",
+              maxWidth: "56px",
+              maxHeight: "56px",
+              bgcolor: "#E6F1F9",
+            }}
+            >
+             <PersonRoundedIcon color="primary" />
+                </Box>
+                <Stack sx={{marginLeft:"30px"}}>
                     <Typography sx={{ fontWeight: "bold"}}>
-                      {item.email}
+                      {admin.name}
                     </Typography>
                     <Typography
                       sx={{
                         color: "gray",
-                        fontWeight: "400",
-                        fontSize: "12px",
+                        fontWeight: "500",
+                        fontSize: "14px",
                       }}
                     >
-                      {item.created_at}
+                      {admin.email}
                     </Typography>
                   </Stack>
-                  <Stack direction="row" spacing={6}>
-                  <Button
-                    sx={{
-                      border: "2px solid #026FC2",
-                      borderRadius: "6px",
-                      width: "66px",
-                      height: "37px",
-                      fontWeight: "600",
-                      fontSize: "14px",
-                      textTransform: "none",
-                    }}
-                    onClick={() => handleClickOpen(item.id)}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                  variant="contained"
-                    sx={{
-                      borderRadius: "6px",
-                      width: "66px",
-                      height: "37px",
-                      fontWeight: "600",
-                      fontSize: "14px",
-                      textTransform: "none",
-                    }}
-                    onClick={() => handleDelete(item.id)}
-                  >
-                    Delete
-                  </Button>
-                  </Stack>
-                </Stack>
-                <Divider light />
-              </>
-            );
-          })}
-      </Stack>
-
-      <Dialog open={open} onClose={handleClose} >
-        <DialogTitle style={{textAlign:"center", fontWeight:"600",color:"#394452"}}>Edit<Box display="inline" style={{color:'#026FC2'}}> Admin</Box>
-        </DialogTitle>
-        <form action='post' onSubmit={handleSubmit}>
-        <DialogContent style={{display:"flex",alignItems:"center",flexDirection:"column",margin:"20px 10px",width:"500px"}}>
-          
-        <TextField
-            autoFocus
-            margin="dense"
-            id="fullName"
-            label="Full Name"
-            type="text"
-            fullWidth
+        </Box>
+        <Box sx={{ display: "flex",alignItems:"space-between" }}>
+          <Button
             variant="outlined"
-            size="30"
-            required  
-            onChange={(event) =>  setFormValues({...formValues, name: event.target.value})}     
-            value={admins.email}       
+            size="small"
+            sx={{
+              border: "2px solid #026FC2",
+              borderRadius: "6px",
+              width: "66px",
+              height: "37px",
+              fontWeight: "600",
+              fontSize: "14px",
+              textTransform: "none",
+              marginRight:"50px"
+            }}
+            onClick={() => handleClickOpen(admin.id)}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "6px",
+              width: "66px",
+              height: "37px",
+              fontWeight: "600",
+              fontSize: "14px",
+              textTransform: "none",
+            }}
+            onClick={() => handleDelete(admin.id)}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Box>
+    ))}
+  </Stack>
+  <Dialog open={open} onClose={handleClose}>
+    <form onSubmit={handleEdit}>
+    <DialogTitle style={{textAlign:"center", fontWeight:"600",color:"#394452"}}>Edit <Box display="inline" style={{color:'#026FC2'}}>Admin</Box></DialogTitle>     
+     <DialogContent >
+        <Stack spacing={4} style={{display:"flex",alignItems:"center",flexDirection:"column",width:"400px"}}>
+          <TextField
+            required
+            sx={{marginTop:"5px"}}
+            fullWidth
+            label="Name"
+            variant="outlined"
+            value={formValues.name}
+            onChange={(event) =>
+              setFormValues({ ...formValues, name: event.target.value })
+            }
           />
           <TextField
-            autoFocus
-            margin="dense"
-            id="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="outlined"
-            size="30"
             required
-            onChange={(event) =>  setFormValues({...formValues, email: event.target.value})}   
-            value={formValues.email }
+            fullWidth
+            label="Email"
+            variant="outlined"
+            value={formValues.email}
+            onChange={(event) =>
+              setFormValues({ ...formValues, email: event.target.value })
+            }
           />
-           
-        </DialogContent>
-        <DialogActions style={{display:"flex",flexDirection:"row", justifyContent:"space-around",marginBottom:"20px"}}>
-        <Button onClick={handleClose} 
-        style={{ backgroundColor: "#FFF", width: "160px",borderRadius: '10px',color:"#026FC2",fontWeight:"600" }}  variant="outlined">Cancel</Button>
+        </Stack>
+      </DialogContent>
+      <DialogActions style={{display:"flex",flexDirection:"row", justifyContent:"space-around",marginBottom:"20px"}}>
+        <Button variant="outlined" onClick={handleClose} style={{ backgroundColor: "#FFF", width: "120px",borderRadius: '10px',color:"#026FC2",fontWeight:"600" }}>Cancel</Button>
+        <Button type="submit"
+         style={{ backgroundColor: "#026FC2",
+          width: "120px",
+          borderRadius: '10px',
+          color:"#FFF",fontWeight:"600" }} variant="outlined">
+          Save
+        </Button>
+      </DialogActions>
+    </form>
+  </Dialog>
+</Box>
+);
+};
 
-        <Button type='submit'
-        style={{ backgroundColor: "#026FC2", width: "160px",borderRadius: '10px',color:"#FFF",fontWeight:"600" }} variant="outlined">Edit Admin</Button>
-
-        </DialogActions>
-        </form>
-      </Dialog>
-    </Box>
-  )
-}
-
-  
-  
+export default AdminsList;
